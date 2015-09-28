760889a7bf764c83227b40d362be6a3d633becf5
jefferton000 <jefferton@gmail.com>
2015-09-27 21:46:52 -0700

    Create worksheet_io.java

---
+++ worksheet_io.java
@@ -0,0 +1,21 @@
+/** Worksheet problems for Java I/O. Refresh Java I/O API knowledge.
+    References lecture. Will possibly contribute to solving the Lab. */
+
+1. Write a method determines whether a file exists:
+public boolean exists(String name) {
+...
+}
+
+2. Write a method that lists the contents of a directory:
+public void ls(String name) {
+...
+}
+
+3. Write a logging method that records a string message to a file, prefixed by a timestamp:
+public void log(String fileName, String msg) {
+...
+}
+
+4. Write a method that reads 4 characters from System.in and prints them out.
+
+5. Write "Hi, Mom!" to a StringWriter and then dump the string to make sure you wrote it.
